87c05cc5f287be0a76ac7875133e40e88122b829
Swan <swan.marin@epitech.eu>
2023-07-05 14:19:59 +0200

    affichage de toute la data

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -7,13 +7,22 @@ class Photographer {
 }
 
 class Media {
-    constructor(id, title, image, likes, date, price) {
-        this.pictureId = id;
-        this.title = title;
-        this.image = image;
-        this.likes = likes;
-        this.date = date;
-        this.price = price;
+    constructor(data) {
+        if (data.image) {
+            this.pictureId = data.id;
+            this.title = data.title;
+            this.image = data.image;
+            this.likes = data.likes;
+            this.date = data.date;
+            this.price = data.price;
+        } else if (data.video) {
+            this.pictureId = data.id;
+            this.title = data.title;
+            this.video = data.video;
+            this.likes = data.likes;
+            this.date = data.date;
+            this.price = data.price;
+        }
     }
 }
 
@@ -56,22 +65,70 @@ function photographerTemplate(data) {
 
 function photographerProfilTemplate(photographer, media) {
     const { id, name, portrait, city, country, tagline, price } = photographer;
-    // const media { id, } = media;
-    const mediaObjects = media.map(
-        (m) => new Media(m.id, m.photographerId, m.title, m.image, m.likes, m.date, m.price)
-      );
-      console.log("IDphotographe :", id);
-      // Affichage des médias
-      console.log("Médias associés :");
-      mediaObjects.forEach((m) => {
-        console.log("ID :", m.pictureId);
-        console.log("Titre :", m.title);
-        console.log("Image :", m.image);
-        console.log("Likes :", m.likes);
-        console.log("Date :", m.date);
-        console.log("Prix :", m.price);
-        console.log("--------------------");
-      });
+    const photographersSection = document.querySelector(".photographer_section");
+    const portfolioSection = document.querySelector(".portfolio-section")
 
+    const mediaObjects = media.map((m) => new Media(m));
+
+    function photographerProfil() {
+        const div = document.createElement('div');
+        photographersSection.appendChild(div);
+
+        const h1 = document.createElement('h1');
+        h1.textContent = name;
       
+        const pCity = document.createElement('p');
+        pCity.textContent = city;
+      
+        const pCountry = document.createElement('p');
+        pCountry.textContent = country;
+
+        div.appendChild(h1);
+        div.appendChild(pCity);
+        div.appendChild(pCountry);
+
+        const divPortrait = document.createElement('div');
+        photographersSection.appendChild(divPortrait);
+
+        const img = document.createElement('img');
+        img.setAttribute('src', `assets/photographers/${portrait}`);
+        img.setAttribute('alt', name);
+
+        divPortrait.appendChild(img);
+    }
+
+    function portfolio() {
+        const div = document.createElement('div');
+        const directoryName = name.split(' ')[0];;
+        portfolioSection.appendChild(div);
+
+        mediaObjects.forEach((m) => {
+            const card = document.createElement('div');
+            const mediaElement = m.image ? document.createElement('img') : document.createElement('video');
+            const name = document.createElement('p');
+
+            if (m.image) {
+                mediaElement.setAttribute('src', `assets/media/${directoryName}/${m.image}`);
+              } else if (m.video) {
+                const source = document.createElement('source');
+                source.setAttribute('src', `assets/media/${directoryName}/${m.video}`);
+                source.setAttribute('type', 'video/mp4');
+                mediaElement.appendChild(source);
+                mediaElement.setAttribute('controls', '');
+                mediaElement.setAttribute('preload', 'metadata');
+              }
+          
+              name.textContent = m.title;
+          
+              card.appendChild(mediaElement);
+              card.appendChild(name);
+          
+              div.appendChild(card);
+          });
+        
+    }
+
+    photographerProfil()
+    portfolio()
+
 }
